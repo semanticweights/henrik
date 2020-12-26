a47e22d129ebae483a64410e8961dd028b2978b3
inejc <nejc.ilenic@gmail.com>
2020-12-26 20:25:39 +0100

    find mlpack

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ An AI bot that plays the Slovenian Tarok card game.
 - `cd henrik/libs/open_spiel`
 - `./install.sh` (make sure to activate the virtual environment first)
 - install [mlpack](https://www.mlpack.org/getstarted.html)
+- build using cmake
--- henrik/src/abstraction.cpp
+++ henrik/src/abstraction.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 
+#include "mlpack/core.hpp"
+#include "mlpack/methods/kmeans/kmeans.hpp"
 #include "open_spiel/games/tarok.h"
 #include "open_spiel/spiel.h"
 
+using namespace open_spiel;
+using namespace mlpack;
+
 int main() {
-  auto game = std::static_pointer_cast<const open_spiel::tarok::TarokGame>(
-      open_spiel::LoadGame("tarok(players=3,rng_seed=0)"));
-  std::cout << game->MaxGameLength() << std::endl;
+  auto game = std::static_pointer_cast<const tarok::TarokGame>(
+      LoadGame("tarok(players=3,rng_seed=0)"));
+  kmeans::KMeans kmeans = kmeans::KMeans();
+
+  std::cout << game->NumPlayers() << std::endl;
 }
